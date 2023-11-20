208fb446a3cf54d76942f5510b136ff6b43cbcea
Shivathmika20 <yelurishivathmika@gmail.com>
2023-11-20 21:15:22 +0530

    styled playlists

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,7 @@ import { useContextProvider } from '../utils/ContextProvider'
 import { reducercase } from '../utils/cases'
 import axios from 'axios';
 
+
 const Body = () => {
   const [{token,selectedPlayId,selectedPlaylist},dispatch]=useContextProvider();
   useEffect(()=>{
@@ -50,17 +51,19 @@ const Body = () => {
                   <div className="image ">
                     <img src={selectedPlaylist.image1} alt="selectedplaylist" className='h-60 shadow-md' />
                   </div>
-                  <div className="playlist_details flex flex-col gap-8">
+                  <div className="playlist_details flex flex-col gap-8 text-[#e0dede]">
                     <span className='text-xl font-semibold'>PLAYLIST</span>
-                    <h2 className='title text-3xl font-bold text-black'>{selectedPlaylist.name}</h2>
+                    <h1 className='title text-7xl font-bold text-white'>{selectedPlaylist.name}</h1>
                     <p className='description'>{selectedPlaylist.description}</p>
                   </div>
               </div>
-              <div className="list">
+              <div className="list mt-8 grid text-[#dddcdc] px-11 py-12 sticky top-[15vh] transition duration-300 ease-in-out ">
+                  
                     <div className="col">
                       <span>#</span>
                     </div>
                     <div className="col">TITLE</div>
+                 
                     <div className="col">ALBUM</div>
                     <div className="col">
                       <span>
@@ -68,29 +71,30 @@ const Body = () => {
                       </span>
                     </div>
               </div> 
-              <div className="tracks">
+              <div className="tracks mx-10 my-3 flex flex-col ">
                         {
                           selectedPlaylist.tracks.map(({id,name,artists,image,duration,album,uri,track_num},index)=>{
                             return(
-                              <div className="rows " index={id}>
-                                <div className="col">
+                              <div className="rows hover:bg-[#000000cc] pb-6 px-2 mb-4" index={id}>
+                                <div className="col flex items-center text-[#dddcdc] ">
                                   <span key={id}>{index+1}</span>
                                 </div>
-                                <div className="detail">
-                                  <div className="img">
+                                
+                                <div className="col detail flex items-center text-[#dddcdc] gap-4 ">
+                                  <div className="img h-10">
                                     <img src={image} alt="tracks" />
                                   </div>
-                                  <div className="info">
-                                   <span className="name">
+                                  <div className="info flex flex-col ">
+                                   <span className="name mt-2 ">
                                     {name}
                                    </span>
                                    <span>{artists}</span>
-                                  </div>
                                 </div>
-                                <div className="album">
+                                </div>
+                                <div className="col album flex items-center text-[#dddcdc]">
                                   {album}
                                 </div>
-                                <div className="time">
+                                <div className="col  time flex items-center text-[#dddcdc]">
                                   {duration}
                                 </div>
                                
